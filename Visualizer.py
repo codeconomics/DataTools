e095ed2449a165dbed7e7c0cf8ba05ce3e65b3b0
Aiden Zhang <zmzhang@brandeis.edu>
2018-06-18 17:43:58 -0400

    debugged, added command line interface

--- Visualizer.py
+++ Visualizer.py
@@ -8,28 +8,47 @@ Created on Fri Jun 15 10:45:02 2018
 import pandas as pd
 import plotly.offline as py
 import plotly.figure_factory as ff
-import click as cli
 import random
 import plotly.graph_objs as go
+import sys
+import ast
+import os.path
 
-#featuredata = pd.read_csv('/Users/zhangzhanming/Desktop/mHealth/MyData/SPADES_2/Derived/Preprocessed/2015/10/08/14/ActigraphGT9X-PostureAndActivity-NA.TAS1E23150066-PostureAndActivity.2015-10-08-14-00-00-000-M0400.feature.csv')
-#annotationdata = pd.read_csv('/Users/zhangzhanming/Desktop/mHealth/MyData/SPADES_2/MasterSynced/2015/10/08/14/SPADESInLab.alvin-SPADESInLab.2015-10-08-14-10-41-252-M0400.annotation.csv')
-#pathout = '/Users/zhangzhanming/Desktop/mHealth/Test/'
-
-
-def annotation_feature_grapher(featuredata, annotationdata, pathout, feature_index=None):
+def annotation_feature_grapher(featuredata, annotationdata, path_out, feature_index=None):
+    """ 
+    
+    Create a figure with selected features(if no feature index is passed, select MEAN
+    STD, MAX by default), and annotation. Create html file in designated file path
+    
+    Args:
+        featuredata: pandas.DataFrame containing feature data
+        annotationdata: pandas.DataFrame containing annotations
+        pathout: path to write the html figure file
+        feature_index: the index of the features to select in featuredata dataframe
+    
+    Returns:
+        the url of the created figure
+    
+    """
+    if isinstance(featuredata, str):
+        featuredata = pd.read_csv(featuredata)     
+    if isinstance(annotationdata, str):
+        annotationdata = pd.read_csv(annotationdata)
+    if isinstance(feature_index, str):
+        feature_index = ast.literal_eval(feature_index)
+    
     # create dict of annotation data
     gantt_df = annotationdata.iloc[:,1:4]
     gantt_df.columns = ['Start','Finish','Task']
     gantt_df['Resource'] = gantt_df['Task']
     colors =  __generate_color(len(gantt_df['Resource'].unique()))
     gantt_fig = ff.create_gantt(gantt_df, group_tasks=True, bar_width=0.7, 
-                                   title='annotation', index_col='Resource',
-                                   colors=colors,show_colorbar=True, width=1200)
+                                   title='Features And Annotation', index_col='Resource',
+                                   colors=colors,show_colorbar=True, width=1300)
     
     # create line chart for selected features
     if feature_index is None:
-        feature_index = [2,3,4] 
+        feature_index = range(2,17)
     
     traces = []
     for index in feature_index:
@@ -39,7 +58,15 @@ def annotation_feature_grapher(featuredata, annotationdata, pathout, feature_ind
                            mode = 'lines+markers')
         traces.append(trace)
     
-    gantt_fig['data'] = gantt_fig['data']+traces
+    newdata = gantt_fig['data']
+    for line in newdata:
+        if 'y' in line:
+            line['y'] = [line['y'][0]+5,line['y'][1]+5]
+        if 'hoverinfo' in line:
+            line['text'] = line['name']
+            line['hoverinfo'] = 'text'
+            
+    gantt_fig['data'] = newdata+traces
     
     # move all the labels up
     newshape = gantt_fig['layout']['shapes']
@@ -47,7 +74,7 @@ def annotation_feature_grapher(featuredata, annotationdata, pathout, feature_ind
         label['y0'] +=5
         label['y1'] +=5
         label['opacity'] = 0.5
-    
+        
     gantt_fig['layout']['shapes'] = newshape
     newyaxis = gantt_fig['layout']['yaxis']
     newrange = newyaxis['range']
@@ -56,9 +83,11 @@ def annotation_feature_grapher(featuredata, annotationdata, pathout, feature_ind
     newyaxis['range'] = [newrange[0], newrange[1]+5]
     gantt_fig['layout']['yaxis'] = newyaxis
     
-    py.plot(gantt_fig, filename=(pathout+'test.html'))
-    
-
+    if os.path.isdir(path_out):
+        path_out = path_out + '/'
+        
+    return py.plot(gantt_fig, filename=(path_out+'test.html'))
+ 
 def __generate_color(n):
     colors = []
     for i in range(n):
@@ -66,5 +95,15 @@ def __generate_color(n):
         g = int(random.random() * 256)
         b = int(random.random() * 256)
         colors.append('rgb('+','.join([str(r),str(g),str(b)])+')') 
-    return colors
+    return colors    
+
+if __name__ == '__main__':
+    if len(sys.argv) <4:
+        print('INSTRUCTION: [feature data file] [annotatoin data file] [pathout] [feature list]')
+    else:
+        if len(sys.argv) == 4:
+            annotation_feature_grapher(sys.argv[1],sys.argv[2],sys.argv[3])
+        else:
+            annotation_feature_grapher(sys.argv[1],sys.argv[2],sys.argv[4])
+
     
